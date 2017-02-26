39a27d39769cfa4acfdd5c29ac1c14d326be5d7e
Jeremy Leif <jeremy.leif@gmail.com>
2017-02-25 19:36:03 -0600

    I think this is what was asked for but the instance wording confused me not sure if I was supposed to use dot notation or do it this way, reguardless i think this answer is correct

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
-
+function PizzaConstructor(state, revenue, locationCount){
+  this.state = state;
+  this.revenue = revenue;
+  this.locationCount = locationCount;
+  this.averageRevenue = function() {
+    var calcAverageRevenue =
+    revenue/ locationCount;
+    return parseFloat(calcAverageRevenue);
+  };
+};
 // object instances
-
+var ilPizza = new PizzaConstructor("IL", 5000, 12);
+var mnPizza = new PizzaConstructor("MN", 300, 3);
+var nvPizza = new PizzaConstructor("NV", 25000, 1);
 // push object instances to locations array
-
+locations.push(ilPizza,mnPizza,nvPizza);
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
   locations.forEach(function (element, index) {
